55738d75ba1520186cce3aa8b92ac0f17556c89d
Critiek <58063875+Critiek@users.noreply.github.com>
2023-06-23 20:11:30 +0930

    Create flag_checker.py

---
+++ flag_checker.py
@@ -0,0 +1,15 @@
+#this code may not work and is just a place-holder for some code I wrote elsewhere. I wrote it on github.........
+
+import time
+
+flag = input("Enter the flag to be checked: ")
+
+while True:
+  if flag == "Flag{Th3_4ns3r_t0_qu3st10n_thr33_1s_Y3S":
+    print("Hooray! You found it!")
+    time.sleep(5)
+    break
+    
+  else:
+    print("Thats not it! Try again!")
+    
